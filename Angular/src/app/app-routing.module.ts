0f66440e8eb6d4a60c810432d999cc9995b7cf4d
Dracnea <nashenfelter@gmail.com>
2020-11-14 14:45:36 -0500

    add product creation

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import {AdminPortalComponent} from './admin-portal/admin-portal.component';
 import {AdminGuard} from './guards/admin.guard';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
+import { ProductCreateComponent } from './product-create/product-create.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full'},
   { path: 'user', component: UserPortalComponent, canActivate: [AuthGuard] },
   {path: 'admin', component: AdminPortalComponent, canActivate: [AuthGuard, AdminGuard]},
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'createProduct', component: ProductCreateComponent, canActivate: [AuthGuard, AdminGuard]}
 ];
 
 @NgModule({
---
+++ Angular/src/app/product-create/product-create.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { ProductService } from '../services/product/product.service';
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+
+  title = 'product-create';
+  form: FormGroup;
+
+  constructor(private readonly fb: FormBuilder, private auth: AuthService, private router: Router, private ps: ProductService) {
+    this.form = this.fb.group({
+      name: ['', [Validators.required]],
+      brand: ['', [Validators.required]]
+    });
+  }
+  
+  ngOnInit(): void {
+  }
+
+  submitCreationForm(): void {
+    if (this.form.valid) {
+      this.ps.postProduct(this.form.getRawValue()).then(product => {
+        if (!product) {
+          console.log(product);
+          console.log("Failed to create Product");
+        } else {
+          console.log("Created Product");
+          this.router.navigate(['/admin', {trigger: 'PRODUCTCREATED'}]);
+        }
+      });
+    }
+  }
+
+}
